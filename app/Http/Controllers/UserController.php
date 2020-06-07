e45525ac653f0d8265cf1d29123893a6c916cf1b
fabioMatosCosta <fabiodanielmc@gmail.com>
2020-06-07 18:46:39 +0200

    create messages controller, users and messages resources

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Users;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
-    public function show(Users $user){
-        return $user;
+    public function show(Users $user): UserResource
+    {
+        return new UserResource($user);
     }
 }
