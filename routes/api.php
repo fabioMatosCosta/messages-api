786d232f8527bafb389e6332c4b7a0a9131e1b14
fabioMatosCosta <fabiodanielmc@gmail.com>
2020-06-13 14:10:40 +0200

    code explaining

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->group(function(){
-    Route::get('/messages/{user}/allmessages', 'Api\v1\MessagesController@show');
+Route::prefix('v1')->group(function(){ // Grouped all routes with a v1 prefix so future versions of the API don't break the applications still using the v1
 
-    Route::get('/messages/{user}/info', 'Api\v1\UserController@show');
+    Route::get('/messages/{user}/allmessages', 'Api\v1\MessagesController@show'); //gets all user messages, {user} is the user id parameter, I assumed the authentication is validated and the user id is accessible
+
+    Route::get('/messages/{user}/info', 'Api\v1\UserController@show'); //gets the user info, for a profile for example, {user} parameter is the same as before
     
-    Route::get('/messages/allusers', 'Api\v1\UserController@index');
+    Route::get('/messages/allusers', 'Api\v1\UserController@index'); // gets all users info in the database, can be used for autocomplete when sending messages
     
-    Route::post('/messages/{user}/send', 'Api\v1\MessagesController@store');
+    Route::post('/messages/{user}/send', 'Api\v1\MessagesController@store'); //posts a new message in the database, {user} parameter is the same as before
+
 });
