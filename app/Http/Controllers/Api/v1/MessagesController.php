75551185857c986cbf93c378dd107e15dab817fc
fabioMatosCosta <fabiodanielmc@gmail.com>
2020-06-13 13:46:28 +0200

    code explaining

--- app/Http/Controllers/Api/v1/MessagesController.php
+++ app/Http/Controllers/Api/v1/MessagesController.php
@@ -14,28 +14,28 @@ class MessagesController extends Controller
     public function show(Messages $user)
     {   
         $messages = Messages::where('recipient_id', $user->id)
-            ->join('users', 'messages.sender_id', '=', 'users.id')
-            ->select('sender_id', 'recipient_id', 'body', 'email', 'first_name', 'messages.created_at as message_created_at')
+            ->join('users', 'messages.sender_id', '=', 'users.id') //joined the sender info to the message table
+            ->select('sender_id', 'recipient_id', 'body', 'email', 'first_name', 'messages.created_at as message_created_at') // select relevant info to display, including the sent date as message_created_at
             ->latest('message_created_at')
             ->get();
         
-        return new MessagesResource($messages);
+        return new MessagesResource($messages); //shows all the messages sorted by sent date, with info of the sender
     }
 
     public function store(Users $user, Request $request)
     {
-        $request->validate([
+        $request->validate([                   //validates the form, throws an error if both fields are not filled in
             'recipient_email' => 'required',
             'body'            => 'required'
         ]);
 
-        $recipient_email = $request->recipient_email;
+        $recipient_email = $request->recipient_email; // get the email of the recipient from the form
 
-        $recipient = Users::where('email', '=', $recipient_email)->firstOrFail();
+        $recipient = Users::where('email', '=', $recipient_email)->firstOrFail(); //finding the recipient email in the database, if not found throws an error
 
         $newMessage = $request->all();
-        $newMessage['sender_id'] = $user->id;
-        $newMessage['recipient_id'] = $recipient->id;
+        $newMessage['sender_id'] = $user->id; // added te id of the sender form the url parameter
+        $newMessage['recipient_id'] = $recipient->id; 
         
         Messages::create($newMessage);
 
